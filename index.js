81d3e3ba13ffcba34c73e7bb60af67974e2492dd
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2026-07-14 14:59:56 +0200

    Write test results to JSON-LD

--- index.js
+++ index.js
@@ -29,42 +29,108 @@ async function extract(inputObj) {
     spaces: 2
   });
 
+  const testSubjects = [];
+
   framed["@graph"].forEach(el => {
     let text = `Enum "${el.diagramLabel}". `;
 
     if (el.apDefinition) {
       const defs = el.apDefinition.map(d => `"${d["@value"]}"`).join(", ");
       text += defs;
+      testSubjects.push({
+        "@type": "TestSubject",
+        "@id": el["@id"],
+        "dcterms:title": `${el.diagramLabel}`,
+        "assertion": {
+          "@type": "Assertion",
+          "test": "_:definition-test",
+          "result": {
+            "@type": "TestResult",
+            "outcome": "passed"
+          }
+        }
+      });
     } else {
       text += "\n  No AP definition provided.\n";
+      const testSubject = {
+        "@type": "TestSubject",
+        "@id": el["@id"],
+        "dcterms:title": `${el.diagramLabel}`,
+        "assertion": {
+          "@type": "Assertion",
+          "test": "_:definition-test",
+          "result": {
+            "@type": "TestResult",
+            "dcterms:description": "No AP definition provided.",
+            "outcome": "failed"
+          }
+        }
+      };
 
       if (el.inverseRange) {
         if (!Array.isArray(el.inverseRange)) {
           el.inverseRange = [el.inverseRange];
         }
 
-        for (let i = 0; i < el.inverseRange.length; i++){
+        let potentialFix = "";
+
+        for (let i = 0; i < el.inverseRange.length; i++) {
           const p = el.inverseRange[i];
           if (i > 0) {
             text += "\n";
+            potentialFix += "\n";
           }
 
           text += "  ";
           const prop = p.assignedURI || p.diagramLabel[0]["@value"];
           text += `${prop} uses this enum`;
+          potentialFix += `${prop} uses this enum`;
 
           if (p.apDefinition && p.apDefinition.length > 0) {
             const propDefs = p.apDefinition.map(d => `"${d["@value"]}"`).join(", ");
             text += ": " + propDefs + " ";
+            potentialFix += ": " + propDefs + " ";
           } else {
             text += ", but no AP definition provided for it.";
+            potentialFix += ", but no AP definition provided for it.";
           }
         }
+
+        testSubject.assertion.potentialFix = potentialFix
       } else {
-        text += "No property found that uses this enum.";
+        const temp = "No property found that uses this enum.";
+        text += temp;
+        testSubject.assertion.potentialFix = temp;
       }
+
+      testSubjects.push(testSubject);
     }
 
     console.log(text);
+  });
+
+  testSubjects.push({
+    "@type": ["TestCriterion", "TestRequirement"],
+    "@id": "_:definition-test",
+    "dcterms:title": "An enum should have a AP definition."
+  });
+
+  const errorsJsonLD = {
+    "@context": {
+      "@vocab": "http://www.w3.org/ns/earl#",
+      "dcterms": "http://purl.org/dc/terms/",
+      "ex": "http://example.com#",
+      "potentialFix": "ex:potentialFix",
+      "assertion": {"@reverse": "subject"},
+      "test": {"@type": "@id"},
+      "outcome": {
+        "@type": "@id"
+      }
+    },
+    "@graph": testSubjects
+  }
+
+  fs.writeJson("errors.jsonld", errorsJsonLD, {
+    spaces: 2
   });
 }
